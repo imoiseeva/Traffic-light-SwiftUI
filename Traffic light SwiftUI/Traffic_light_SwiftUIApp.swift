8438e33e052f2cea78e3c888d934500ab827c878
imoiseeva <moiseeva.irina@gmail.com>
2021-03-25 10:24:13 +0200

    Initial Commit

---
+++ Traffic light SwiftUI/Traffic_light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_light_SwiftUIApp.swift
+//  Traffic light SwiftUI
+//
+//  Created by Irina Moiseeva on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
